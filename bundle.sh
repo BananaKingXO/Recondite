52ed9dd15c313e1607197939a7faf68b56e7a345
BananaKingXO <BananKingVr@gmail.com>
2026-07-23 18:09:07 -0500

    ✨ Added mod bundling and versioning

--- bundle.sh
+++ bundle.sh
@@ -1,8 +1,22 @@
+read -p "Version: " version
 
-rm -rf artifacts
-mkdir -p artifacts
+echo "Checking mods metadata:"
+grep -r ${version} mod/
+
+read -p "Do you see ${version} in the above list? If not update your mods metadata. "
+
+rm -rf artifacts/${version}
+mkdir -p artifacts/${version}
 
 cd datapack/
-zip -r ../artifacts/Spellbooks.zip data/ pack.mcmeta
+zip -r ../artifacts/${version}/Spellbooks-${version}.zip data/ pack.mcmeta
+cp ../artifacts/${version}/Spellbooks-${version}.zip ../artifacts/${version}/mod.zip
+
+cd ../resource_pack
+zip -r ../artifacts/${version}/mod.zip assets/
+cd ../mod
+zip -r ../artifacts/${version}/mod.zip *
+cd ../artifacts/${version}
+mv mod.zip Spellbooks-${version}.jar
 
 cd ..
